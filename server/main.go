2309353ee0c72c774404bc3f5b7e261fc916e678
Marcos Hack <marcoshack@gmail.com>
2015-08-20 11:46:32 -0300

    Use gorilla/mux router

--- server/main.go
+++ server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/marcoshack/schedula"
 )
 
@@ -14,20 +16,28 @@ const (
 
 func main() {
 	log.Printf("Schedula Server %s", version)
-	httpServer := &http.Server{
-		Addr: "127.0.0.1:8080",
-	}
+
+	serverAddr := flag.String("b", "127.0.0.1:8080", "HTTP bind address")
+	numberOfWorkers := flag.Int("w", 50, "number of workers to execute callback requests")
+	flag.Parse()
 
 	repository, err := schedula.NewRepository()
-	scheduler, err := schedula.InitAndStartScheduler(repository, map[string]interface{}{})
+	scheduler, err := schedula.InitAndStartScheduler(repository, schedula.SchedulerConfig{
+		NumberOfWorkers: *numberOfWorkers,
+	})
 	if err != nil {
 		log.Fatalf("schedula: error initializing scheduler: %s", err)
 	}
 
-	http.Handle(jobsPath, &JobsHandler{scheduler: scheduler, Path: jobsPath})
+	jobsHandler := &JobsHandler{scheduler: scheduler, Path: jobsPath}
 
-	log.Printf("Listening on %s", httpServer.Addr)
-	log.Fatal(httpServer.ListenAndServe())
+	router := mux.NewRouter()
+	router.HandleFunc("/jobs/", jobsHandler.List).Methods("GET")
+	router.HandleFunc("/jobs/", jobsHandler.Create).Methods("POST")
+	router.HandleFunc("/jobs/{id}", jobsHandler.Find).Methods("GET")
+
+	log.Printf("Listening on %s", *serverAddr)
+	log.Fatal(http.ListenAndServe(*serverAddr, router))
 
 	scheduler.Stop()
 }
